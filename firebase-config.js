ad5ce218f2f56085f6a3642f15e2e494021723e2
Taruto3 <hafuxu1227@gmail.com>
2026-07-26 17:08:18 +0900

    Firebaseの接続設定を追加
    Firebaseの接続設定を追加

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Firebase Consoleの「プロジェクトの設定」→「マイアプリ」に表示される値へ置き換えてください。
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCkH0P4beRZnezoCq-mewHlUTeIMzJQc9g",
+  authDomain: "shopping-list-taruto3.firebaseapp.com",
+  projectId: "shopping-list-taruto3",
+  storageBucket: "shopping-list-taruto3.firebasestorage.app",
+  messagingSenderId: "784071661191",
+  appId: "1:784071661191:web:89ae8af1e52544874bca22"
 };
